cfffc900cb23b88c2f57bbbc3603abe7b2acdb1c
Maxi <maxifeldman@hotmail.com>
2018-12-27 22:15:15 -0300

    now PUT, DELETE, GET, POST are avaliable and usable

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,13 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de Datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
+urlDB = 'mongodb://cafe-test:asd123@ds245234.mlab.com:45234/cafe-restserver'
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,48 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 require('./config/config');
 
 //.use: cada vez que se hace una peticion (ya sea GET, POST, etc.) se ejecutan estas funciones (se llaman middleware).
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-//GET: Obtener informacion de la base de datos.
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('DataBase ONLINE');
 });
 
-//POST: Crear registros en la base de datos.
-app.post('/usuario', function(req, res) {
-    let person = req.body;
-
-    if (person.nombre == undefined) {
-        res.status(400).json({
-            ok: false,
-            message: "Error: There was no given name."
-        });
-    } else {
-        res.json({
-            person
-        });
-
-    }
-
-})
-
-//PUT: Actualizar registros de la base de datos. (casi igual que el patch)
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id; //para obtener el id mediante la peticion HTTP. (ver que "params.id" se relaciona con el :id del primer argumento).
-    res.json({
-        id
-    });
-})
-
-//DELETE: 'eliminar' registros de la base de datos. (Usualmente en realidad es actualizar un estado para decir que no esta mas disponible el registro).
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-})
 app.listen(process.env.PORT, () => console.log("listening on port", process.env.PORT));
